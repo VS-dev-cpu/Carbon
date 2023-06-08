698334beb736f94e51041a12289b260af308974c
SamTheDev <vs.samu.dev@gmail.com>
2023-06-08 19:33:54 +0200

    added logger

---
+++ include/Carbon/debug/log.h
@@ -0,0 +1,24 @@
+#ifndef _LOG_H_
+#define _LOG_H_
+
+#include <stdarg.h>
+
+// Print a Custom Log
+void log_vcustom(char type, const char *tag, const char *message, va_list args);
+
+// Print a Custom Log
+void log_custom(char type, const char *tag, const char *message, ...);
+
+// Print a System Message
+void log_system(const char *tag, const char *message, ...);
+
+// Print a Debug Message
+void log_debug(const char *tag, const char *message, ...);
+
+// Print a Warning Message
+void log_warning(const char *tag, const char *message, ...);
+
+// Print an Error Message
+void log_error(const char *tag, const char *message, ...);
+
+#endif
---
+++ src/debug/log.c
@@ -0,0 +1,74 @@
+#include <Carbon/debug/log.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+// Get Time as String
+void stime(char *str) {
+    time_t rawtime;
+    struct tm *timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    sprintf(str, "%d.%d.%d %d:%d:%d", timeinfo->tm_year + 1900,
+            timeinfo->tm_mon + 1, timeinfo->tm_mday, timeinfo->tm_hour,
+            timeinfo->tm_min, timeinfo->tm_sec);
+}
+
+void log_vcustom(char type, const char *tag, const char *message,
+                 va_list args) {
+    // TODO: tag cutting
+
+    char msg[1024];
+    char date[32];
+    stime(date);
+
+    sprintf(msg, "[%s] %s [%c]  %s\n", date, tag, type, message);
+    vprintf(msg, args);
+}
+
+void log_custom(char type, const char *tag, const char *message, ...) {
+    va_list args;
+    va_start(args, message);
+
+    log_vcustom(type, tag, message, args);
+
+    va_end(args);
+}
+
+void log_system(const char *tag, const char *message, ...) {
+    va_list args;
+    va_start(args, message);
+
+    log_vcustom('S', tag, message, args);
+
+    va_end(args);
+}
+
+void log_debug(const char *tag, const char *message, ...) {
+    va_list args;
+    va_start(args, message);
+
+    log_vcustom('D', tag, message, args);
+
+    va_end(args);
+}
+
+void log_warning(const char *tag, const char *message, ...) {
+    va_list args;
+    va_start(args, message);
+
+    log_vcustom('W', tag, message, args);
+
+    va_end(args);
+}
+
+void log_error(const char *tag, const char *message, ...) {
+    va_list args;
+    va_start(args, message);
+
+    log_vcustom('E', tag, message, args);
+
+    va_end(args);
+}
